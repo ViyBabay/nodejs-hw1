9c5cd75da55f14cbc5d3c5bf4dfd346145177448
Александр Пикуш <pikush.oleksandr@gmail.com>
2023-08-15 22:03:13 +0300

    ADD: CLI-app

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
+const { join } = require("path");
+const contactsPath = join(__dirname, "db/contacts.json");
+
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
+
+const getContactById = async (id) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === id);
+  return result || null;
+};
+
+const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const removeContact = async (id) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
---
+++ index.js
@@ -0,0 +1,50 @@
+const contactsService = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.table(allContacts);
+    case "get":
+      const getContact = await contactsService.getContactById(id);
+      return console.log(getContact);
+    case "add":
+      const addNewContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(addNewContact);
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      return console.log(deleteContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "05olLMgyVQdWRwgKfg5J6" });
+// invokeAction({
+//   action: "add",
+//   name: "go",
+//   email: "sgo@gmail.com",
+//   phone: "222-22-22",
+// });
+// invokeAction({ action: "remove", id: "NyP34q3MltuoiXwZwVR-H" });
